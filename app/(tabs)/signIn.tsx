e6f13218f752641ee0fa6e74aef423300dfbb83a
enkhtuya0511 <149783431+enkhtuya0511@users.noreply.github.com>
2024-08-17 23:03:38 +0800

    login logic

--- app/(tabs)/signIn.tsx
+++ app/(tabs)/signIn.tsx
@@ -1,8 +1,21 @@
 import { Link } from "expo-router";
-import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard, Pressable } from "react-native";
+import { useState } from "react";
+import { useAuth } from "@/contexts/AuthProvider";
+import { View, Text, TextInput, TouchableWithoutFeedback, Keyboard, Pressable, ActivityIndicator } from "react-native";
 import { styles } from "@/styles/signIn-style";
 
 export default function signIn() {
+  const { onLogin, loading } = useAuth();
+  const [email, onChangeEmail] = useState<string | undefined>();
+  const [password, onChangePassword] = useState<string | undefined>();
+
+  const loginHandler = () => {
+    if (email && password)
+      onLogin({
+        email,
+        password,
+      });
+  };
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={styles.container}>
@@ -11,13 +24,14 @@ export default function signIn() {
             <Text style={styles.logo}>🌱</Text>
             <TextInput
               style={styles.input}
-              onChangeText={() => console.log("input")}
               placeholder="Мэйл хаяг"
               placeholderTextColor={"#828282"}
+              onChangeText={onChangeEmail}
+              keyboardType="email-address"
             />
             <TextInput
               style={styles.input}
-              onChangeText={() => console.log("input")}
+              onChangeText={onChangePassword}
               placeholder="Нууц үг"
               placeholderTextColor={"#828282"}
               secureTextEntry={true}
@@ -26,9 +40,15 @@ export default function signIn() {
               <Text style={styles.forgotPass}>Нууц үгээ мартсан уу?</Text>
             </Pressable>
           </View>
-          <Pressable style={styles.button}>
-            <Text style={styles.buttonText}>Нэвтрэх</Text>
-          </Pressable>
+          {loading ? (
+            <View style={styles.button}>
+              <ActivityIndicator size="large" color={"#20222a"} />
+            </View>
+          ) : (
+            <Pressable style={styles.button} onPress={loginHandler}>
+              <Text style={styles.buttonText}>Нэвтрэх</Text>
+            </Pressable>
+          )}
         </View>
         <Link href={"/signUp"}>
           <Text style={{ color: "#b3b3b3", marginTop: 20 }}>
